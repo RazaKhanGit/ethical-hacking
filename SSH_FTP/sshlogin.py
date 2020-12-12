bbc8ef0c7d519d04768b0cf74f711c760f2fcf00
Raza Khan <mdrazakhan2019@gmail.com>
2020-12-12 16:28:48 +0530

    Add send_command() function to script

--- SSH_FTP/sshlogin.py
+++ SSH_FTP/sshlogin.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python
 
-import pexpect
+import wexpect # for window use wexpect, for linux use pexpect
 PROMPT = ['# ', '>>> ', '> ', '\$ ']
+
+def send_command(child, command):
+    child.sendline(command)
+    child.expect(PROMPT)
+    print(child.before)
+
 
 def connect(user, host, password):
     ssh_newkey = 'Are you sure you want to continue connecting'
     connStr = 'ssh ' + user + '@' + host
-    child  = pexpect.spawn(connStr)
-    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword: '])
+    child  = wexpect.spawn(connStr)
+    print(child)
+    ret = child.expect([wexpect.TIMEOUT, ssh_newkey, '[P|p]assword: '])
     if ret == 0:
         print('[-] Error Connecting')
-        return
+        # return
     if ret == 1:
         child.send('yes')
-        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword: '])
+        ret = child.expect([wexpect.TIMEOUT, '[P|p]assword: '])
         if ret == 0:
             print('[-] Error Connecting')
-            return
-    child.sendLine(password)
+            # return
+    child.sendline(password)
     child.expect(PROMPT)
     return child
 
 
 def main():
-    host = '192.168.1.5'
-    user = 'msfadmin'
-    password = 'msfadmin' # for metaspoitable host
+    host = input("Enter the Host to Target: ")
+    user = input("Enter SSH username: ")
+    password = input("Enter SSH password: ")
     child = connect(user, host, password)
-    # send_command(child, 'cat /etc/shadow | grep root;ps')
+    send_command(child, 'ls;ps')
 
 main()
